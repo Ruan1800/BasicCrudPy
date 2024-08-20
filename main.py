a49ca4d1eb18274043b14b9426bf36fed334fd69
Ruan <rrjalfim@gmail.com>
2024-08-19 23:20:24 -0300

    começando meu crud basico em python

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+def menu():
+    print("Escolha uma opção:\n")
+    print("1 - Criar usuario")
+    print("2 - Deletar usuario")
+    print("3 - Atualizar usuario")
+    print("4 - Listar usuario")
+    return int(input("Digite um numero da opçao: "))
+    
+    
+    
+    
+def main():
+    menu()
+    
+
+
+if __name__=="__main__":
+    main()
