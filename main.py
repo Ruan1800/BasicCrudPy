0d2afa8552929547c818ccf72d63582938653cf1
Ruan <rrjalfim@gmail.com>
2024-08-25 16:55:22 -0300

    começando a fazer o create e o delete do crud

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-
-
-
-
-
-
 def menu():
     print("Escolha uma opção:\n")
     print("1 - Criar usuario")
     print("2 - Deletar usuario")
     print("3 - Atualizar usuario")
     print("4 - Listar usuario")
-    return int(input("Digite um numero da opçao: "))
+    global menu_value
+    menu_value = int(input("Digite um numero da opçao: "))
+    return menu_value
     
-    
-    
+def check_option(option):
+    if option < 1 or option > 4:
+         return True
     
 def main():
-    menu()
-    
-
+    while check_option(menu()):
+        print("valor Invalido")
+        
+       
 
 if __name__=="__main__":
     main()
